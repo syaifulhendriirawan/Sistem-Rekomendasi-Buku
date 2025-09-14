d31c7347927ee0e932f7b9f6d27d69839aacecc6
SYAIFUL HENDRI IRAWAN <syaifulhendriirwn@gmail.com>
2025-09-14 21:31:18 +0700

    Menambahkan tampilan web menggunakan streamlit

--- rekomendasi_buku/components/stage_01_data_validation.py
+++ rekomendasi_buku/components/stage_01_data_validation.py
@@ -20,8 +20,8 @@ class DataValidation:
     
     def preprocess_data(self):
         try:
-            ratings = pd.read_csv(self.data_validation_config.ratings_csv_file, sep=";", error_bad_lines=False, encoding='latin-1')
-            books = pd.read_csv(self.data_validation_config.books_csv_file, sep=";", error_bad_lines=False, encoding='latin-1')
+            ratings = pd.read_csv(self.data_validation_config.ratings_csv_file, sep=";", on_bad_lines='skip', encoding='latin-1')
+            books = pd.read_csv(self.data_validation_config.books_csv_file, sep=";", on_bad_lines='skip', encoding='latin-1')
             
             logging.info(f" Shape of ratings data file: {ratings.shape}")
             logging.info(f" Shape of books data file: {books.shape}")
